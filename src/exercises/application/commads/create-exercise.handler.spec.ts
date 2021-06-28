5f45959b9cf866001438084e0f7999d512967ed3
Gonzalo Barba <zalo250@gmail.com>
2021-06-28 17:14:10 +0200

    Add test

--- src/exercises/application/commads/create-exercise.handler.spec.ts
+++ src/exercises/application/commads/create-exercise.handler.spec.ts
@@ -1,6 +1,44 @@
-// import { CreateExerciseHandler } from "./create-exercise.handler";
-//
-// describe("CreateExerciseHandler", () => {
-//   let handler: CreateExerciseHandler;
-//   let repository:
-// })
+import { Test, TestingModule } from "@nestjs/testing";
+import { Repository } from "typeorm";
+
+import { CreateExerciseHandler } from "./create-exercise.handler";
+import { Exercise } from "../../domain/entities/exercise.orm-entity";
+import { CreateExerciseCommand } from "./create-exercise.command";
+import { NotFoundException } from "@nestjs/common";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+describe("CreateExerciseHandler", () => {
+    let repository: Repository<Exercise>;
+    let createExerciseHandler: CreateExerciseHandler;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CreateExerciseHandler,
+                CreateExerciseCommand,
+                {
+                    provide: getRepositoryToken(Exercise),
+                    useClass: Repository
+                }
+            ]
+        }).compile();
+        repository = module.get<Repository<Exercise>>(
+            getRepositoryToken(Exercise)
+        );
+    });
+
+    describe("execute", () => {
+        it("should throw NotFoundException when user is not found", async () => {
+            const command = new CreateExerciseCommand(
+                "user-id-122",
+                "Test exercise content"
+            );
+
+            repository.find = jest.fn().mockResolvedValue(undefined);
+
+            await expect(
+                createExerciseHandler.execute(command)
+            ).rejects.toThrow(NotFoundException);
+        });
+    });
+});
